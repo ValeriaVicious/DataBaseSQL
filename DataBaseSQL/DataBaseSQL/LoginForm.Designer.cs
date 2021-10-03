0387eb2c1ac2fa8fd6ffb275c4ca599406861e94
Valeria Vicious <59263802+ValeriaVicious@users.noreply.github.com>
2021-10-03 21:29:04 +0200

    Реализация окна авторизации

---
+++ DataBaseSQL/DataBaseSQL/LoginForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace DataBaseSQL
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toCloseFormAutorizationLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.rememberCheckBox = new System.Windows.Forms.CheckBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel1.Controls.Add(this.toCloseFormAutorizationLabel);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(354, 180);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseMove);
+            // 
+            // toCloseFormAutorizationLabel
+            // 
+            this.toCloseFormAutorizationLabel.AutoSize = true;
+            this.toCloseFormAutorizationLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.toCloseFormAutorizationLabel.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toCloseFormAutorizationLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.toCloseFormAutorizationLabel.Location = new System.Drawing.Point(326, 0);
+            this.toCloseFormAutorizationLabel.Name = "toCloseFormAutorizationLabel";
+            this.toCloseFormAutorizationLabel.Size = new System.Drawing.Size(24, 27);
+            this.toCloseFormAutorizationLabel.TabIndex = 2;
+            this.toCloseFormAutorizationLabel.Text = "x";
+            this.toCloseFormAutorizationLabel.Click += new System.EventHandler(this.ToCloseFormAutorizationLabel_Click);
+            this.toCloseFormAutorizationLabel.MouseEnter += new System.EventHandler(this.ToCloseFormAutorizationLabel_MouseEnter);
+            this.toCloseFormAutorizationLabel.MouseLeave += new System.EventHandler(this.ToCloseFormAutorizationLabel_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Georgia", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(99, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Авторизация";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DataBaseSQL.Properties.Resources.user_6380868_1280;
+            this.pictureBox1.Location = new System.Drawing.Point(84, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(184, 124);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginTextBox.Location = new System.Drawing.Point(33, 203);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(293, 20);
+            this.loginTextBox.TabIndex = 1;
+            this.loginTextBox.Text = "Введите свой логин...";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordTextBox.Location = new System.Drawing.Point(33, 247);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(293, 20);
+            this.passwordTextBox.TabIndex = 2;
+            this.passwordTextBox.Text = "password";
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // rememberCheckBox
+            // 
+            this.rememberCheckBox.AutoSize = true;
+            this.rememberCheckBox.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rememberCheckBox.Location = new System.Drawing.Point(33, 284);
+            this.rememberCheckBox.Name = "rememberCheckBox";
+            this.rememberCheckBox.Size = new System.Drawing.Size(178, 27);
+            this.rememberCheckBox.TabIndex = 3;
+            this.rememberCheckBox.Text = "Запомнить меня";
+            this.rememberCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.CornflowerBlue;
+            this.loginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loginButton.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginButton.Location = new System.Drawing.Point(104, 333);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(136, 40);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 401);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.rememberCheckBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginTextBox);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.CheckBox rememberCheckBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label toCloseFormAutorizationLabel;
+    }
+}
