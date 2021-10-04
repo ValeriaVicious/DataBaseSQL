f1cfff3ae1cb60da2bb81fa61f9370f05b7a37f5
Valeria Vicious <59263802+ValeriaVicious@users.noreply.github.com>
2021-10-04 16:40:53 +0200

    Описан класс DataBase, содержащий в себе все необходимые характеристики для работы с DataBase

---
+++ DataBaseSQL/DataBaseSQL/Code/DataBase.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+
+
+namespace DataBaseSQL
+{
+    internal sealed class DataBase
+    {
+        #region Fields
+
+        private MySqlConnection _connection = new MySqlConnection(Constants.TheFollowingConnectionParameters);
+
+        #endregion
+
+
+        #region Methods
+
+        public void OpenConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Open)
+            {
+                _connection.Close();
+            }
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return _connection;
+        }
+
+        #endregion
+    }
+}
